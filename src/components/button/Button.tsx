a32e17403fcd30e0f52b7414f216459f758b6a87
Евгений Кириченко <zheniakk@gmail.com>
2020-06-18 15:06:15 +0300

    added button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {Button as MaterialButton} from "@material-ui/core";
+
+interface ButtonProps {
+    text: string;
+}
+
+const useStyles = makeStyles(() => ({
+    root: {
+        height: "40px",
+        background: "#FFFFFF",
+        border: "1px solid #000000",
+        boxSizing: "border-box",
+        borderRadius: "34px",
+        fontSize: "12px",
+        lineHeight: "15px",
+        fontWeight: 500,
+        padding: "6px 18px"
+    }
+}));
+
+const Button = (props: ButtonProps) => {
+    const classes = useStyles();
+    const {text} = props;
+
+    return (
+        <MaterialButton
+            classes={{
+                root: classes.root
+            }}
+        >
+            {text}
+        </MaterialButton>
+    );
+};
+
+export default Button;
